bdfe5a400854cd3c227a6b88e17fb45d5e956f66
Markus Dick <47363631+MarkusDick@users.noreply.github.com>
2025-02-11 10:41:23 +0100

    feat: add ScrollMessageBox

---
+++ SidderPlus/ScrollMessageBox.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml.Linq;
+
+namespace SidderApp
+{
+    public partial class ScrollMessageBox : Form
+    {
+        public ScrollMessageBox(string title, string message)
+        {
+            InitializeComponent();
+            this.Text = title;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
+            this.ShowIcon = false;
+            this.ShowInTaskbar = false;
+            richTextBox1.Text = message;
+        }
+    }
+}
